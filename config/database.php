7de5a0d29789c350636c332c0d6ab130e30c04df
ankitjain28may <ankitjain28may77@gmail.com>
2017-03-12 00:04:21 +0530

    Fixed build

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => env('DB_HOST_SQL', '127.0.0.1'),
+            'port' => env('DB_PORT_SQL', '3306'),
+            'database' => env('DB_DATABASE_SQL', 'forge'),
+            'username' => env('DB_USERNAME_SQL', 'forge'),
+            'password' => env('DB_PASSWORD_SQL', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
@@ -55,11 +55,11 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => env('DB_HOST_PSQL', '127.0.0.1'),
+            'port' => env('DB_PORT_PSQL', '5432'),
+            'database' => env('DB_DATABASE_PSQL', 'forge'),
+            'username' => env('DB_USERNAME_PSQL', 'forge'),
+            'password' => env('DB_PASSWORD_PSQL', ''),
             'charset' => 'utf8',
             'prefix' => '',
             'schema' => 'public',
