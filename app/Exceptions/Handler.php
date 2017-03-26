0c45d1bb420452036d51faf8852c5ecc925f92dc
ankitjain28may <ankitjain28may77@gmail.com>
2017-03-26 17:54:12 +0530

    Added error page

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -5,6 +5,7 @@ namespace App\Exceptions;
 use Exception;
 use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use File;
 
 class Handler extends ExceptionHandler
 {
@@ -44,7 +45,10 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
-        return parent::render($request, $exception);
+        // return parent::render($request, $exception);
+        return response()->view('error', [], 500);
+
+
     }
 
     /**
